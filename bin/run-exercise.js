86f8503cb29846ac41ee3baeb2830faf4bdcc477
Jack Franklin <jack@jackfranklin.net>
2018-08-11 20:51:12 +0100

    some error handling

--- bin/run-exercise.js
+++ bin/run-exercise.js
@@ -1,6 +1,7 @@
 const Bundler = require('parcel-bundler')
 const path = require('path')
 const chalk = require('chalk')
+const fs = require('fs')
 
 const { argv } = require('yargs')
 
@@ -8,7 +9,10 @@ const folderName = argv._[0]
 const exerciseNumber = argv._[1]
 
 if (!exerciseNumber || !folderName) {
-  console.log(chalk.red('You forgot the exercise number and/or folder name.'))
+  console.log(
+    'ERROR',
+    chalk.red('You forgot the exercise number and/or folder name.')
+  )
   process.exit(1)
 }
 
@@ -18,6 +22,14 @@ const exercisePath = path.join(
   String(exerciseNumber),
   'index.html'
 )
+
+const pathExists = fs.existsSync(exercisePath)
+
+if (!pathExists) {
+  console.log('ERROR', chalk.red(`${exercisePath} does not exist.`))
+  console.log('\n')
+  process.exit(1)
+}
 
 const file = path.join(process.cwd(), exercisePath)
 
