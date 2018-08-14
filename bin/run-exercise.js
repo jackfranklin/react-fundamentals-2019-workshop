84a35de7f419811978fb5573e1913f775ca1f98f
Jack Franklin <jack@jackfranklin.net>
2018-08-14 22:29:42 +0100

    attempting to automate index.html

--- bin/run-exercise.js
+++ bin/run-exercise.js
@@ -5,6 +5,36 @@ const fs = require('fs')
 
 const { argv } = require('yargs')
 
+const createHtml = (folderName, exerciseNumber) => `
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="../../common.css" />
+    <title>Exercise: ${folderName} ${exerciseNumber}</title>
+  </head>
+<body>
+  <h1>Exercise: ${folderName} ${exerciseNumber}</h1>
+  <div id="react-root"></div>
+  <script src="./${exerciseNumber}.js"></script>
+</body>
+</html>`
+
+const getExercisePath = (folderName, exerciseNumber) =>
+  path.join('exercises', folderName, String(exerciseNumber))
+
+const generateIndexFile = (folderName, exerciseNumber) => {
+  const filePath = path.join(
+    getExercisePath(folderName, exerciseNumber),
+    'index.html'
+  )
+
+  if (fs.existsSync(filePath)) return
+
+  const contents = createHtml(folderName, exerciseNumber)
+
+  console.log('Generating index.html file for exercise')
+  fs.writeFileSync(filePath, contents, { encoding: 'utf8' })
+}
+
 const folderName = argv._[0]
 const exerciseNumber = argv._[1]
 
@@ -16,12 +46,7 @@ if (!exerciseNumber || !folderName) {
   process.exit(1)
 }
 
-const exercisePath = path.join(
-  'exercises',
-  folderName,
-  String(exerciseNumber),
-  'index.html'
-)
+const exercisePath = getExercisePath(folderName, exerciseNumber)
 
 const pathExists = fs.existsSync(exercisePath)
 
@@ -30,6 +55,8 @@ if (!pathExists) {
   console.log('\n')
   process.exit(1)
 }
+
+generateIndexFile(folderName, exercisePath)
 
 const file = path.join(process.cwd(), exercisePath)
 
