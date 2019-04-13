95e9f1f841734262e6686ead71cb11716857f108
Jack Franklin <jack@jackfranklin.net>
2019-04-13 20:02:55 +0100

    12

--- exercises/react/12/12.js
+++ exercises/react/12/12.js
@@ -1,40 +1,47 @@
 import ReactDOM from 'react-dom'
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import fetch from 'so-fetch-js'
 
-class Post extends Component {
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-  }
+const Post = props => {
+  const urlForPost = `https://jsonplaceholder.typicode.com/posts/${props.id}`
 
-  state = {
-    post: null,
-  }
+  const [post, setPost] = useState(null)
 
-  componentDidMount() {
-    const urlForPost = `https://jsonplaceholder.typicode.com/posts/${
-      this.props.id
-    }`
+  useEffect(() => {
+    console.log('I get run on every render')
 
-    fetch(urlForPost).then(response => {
-      const post = response.data
-      this.setState({ post })
-    })
-  }
+    // let this code run to see the increment looping
+    // setPost({
+    //   title: 'testing',
+    //   body: 'testing',
+    // })
 
-  render() {
-    return this.state.post ? (
-      <div>
-        <h1>{this.state.post.title}</h1>
-        <p>{this.state.post.body}</p>
-      </div>
-    ) : (
-      <p>Loading!</p>
-    )
-  }
+    // TODO: run this code to fetch the post ensuring that you add the empty
+    // dependencies array to the useEffect call so it doesn't cause an infinite
+    // loop!
+
+    // NOTE: you will see an eslint warning when you do this, but that's fine.
+    // We're going to chat about why that is shortly 👌
+
+    // fetch(urlForPost).then(response => {
+    // response.data is the post
+    // })
+  })
+
+  return post ? (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  ) : (
+    <p>Loading the post!</p>
+  )
 }
 
+Post.propTypes = {
+  id: PropTypes.number.isRequired,
+}
 const App = () => {
   return <Post id={1} />
 }
