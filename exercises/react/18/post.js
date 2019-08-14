b83d2b4128e533b97d8fe62f2e91f7b646fe24f7
Jack Franklin <jack@jackfranklin.net>
2019-08-14 10:45:19 +0100

    styling

--- exercises/react/18/post.js
+++ exercises/react/18/post.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 /* TODO: update this compoonent so when you click on its title, it shows a <div> with the post's contents inside
+ * hint: the post's contents is in props.post.body
  * give the post div the class of "post-contents"
  */
 
 const Post = props => {
   return (
-    <div className="post" className="post-wrapper">
+    <div className="post">
       <a href="">{props.post.title}</a>
       <span>Posted on {props.post.date}</span>
     </div>
