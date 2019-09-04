a45c9a243129af3cfa547b02ef287515e14e2c48
Jack Franklin <jack@jackfranklin.net>
2019-09-04 09:46:24 +0100

    tweaks

--- exercises/react/18/post.js
+++ exercises/react/18/post.js
@@ -5,10 +5,16 @@ import React from 'react'
  * give the post div the class of "post-contents"
  */
 
+const onTitleClick = event => {
+  event.preventDefault()
+}
+
 const Post = props => {
   return (
     <div className="post">
-      <a href="">{props.post.title}</a>
+      <a href="#" onClick={onTitleClick}>
+        {props.post.title}
+      </a>
       <span>Posted on {props.post.date}</span>
     </div>
   )
