34f5d0524c39cf47b45b958c27a1f64932f4c763
Jack Franklin <jack@jackfranklin.net>
2019-12-09 09:17:53 +0000

    bump date-fns

--- exercises/utils.js
+++ exercises/utils.js
@@ -3,5 +3,5 @@ import { format } from 'date-fns'
 
 export const getRandomDate = () => {
   // https://date-fns.org/v1.30.1/docs/format
-  return format(faker.date.recent(100), 'GGGG-MM-DD')
+  return format(faker.date.recent(100), 'GGGG-MM-dd')
 }
